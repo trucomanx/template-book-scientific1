37e5bc8094e55294e666cf50c621fe434a20a274
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2020-10-23 17:06:38 -0300

    Primera version

---
+++ clean.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+filename=main
+
+rm -rf $filename
+
+
+rm -f abc-*.*
+
+rm -f */*/*-epspdf-to.pdf
+rm -f */*/*converted-to.pdf
+rm -f */*-epspdf-to.pdf
+rm -f */*converted-to.pdf
+rm -f */*/*.backup
+rm -f */*/*.aux 
+rm -f */*.backup
+rm -f */*.aux 
+
+rm -f *.mx1
+rm -f *.idx
+rm -f *.ind
+rm -f *.aux 
+rm -f *.log 
+rm -f *.bcf 
+rm -f *.out 
+rm -f *.ptc 
+rm -f *.run.xml 
+rm -f *.toc 
+rm -f *.nlo 
+rm -f *.nlg 
+rm -f *.nls 
+rm -f *.lot 
+rm -f *.lof 
+rm -f *.blg 
+rm -f *.bbl 
+rm -f *.ilg
+rm -f *.backup
+
+#rm -f main.loe
+rm -f main.MisDefinicoes
+rm -f main.MisExample
+rm -f main.MisExercise
+rm -f main.MisTheorem
+rm -f main.MisNotation
+rm -f main.MisElaboraciones
+rm -f main.MisFrases
+rm -f main.MisInformacoes
+rm -f main.mw
+
---
+++ exec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#pdflatex -interaction=nonstopmode cover.tex
+#pdflatex -interaction=nonstopmode cover.tex
+#pdflatex -interaction=nonstopmode cover.tex
+
+
+#for VARIABLE in 1 # 2 ##habilitar para corregir paginas glosario
+#do
+	pdflatex -shell-escape main
+	makeindex main.idx -s StyleInd.ist
+	biber main
+	pdflatex -shell-escape main
+	makeindex main.idx -s StyleInd.ist
+	pdflatex -shell-escape main x 2
+#done
+
+#mv -f main.pdf samba-gafieira.pdf
+
+ play -n synth 1 sine 880 vol 0.5 #printf '\a' #tput bel
+./clean.sh
