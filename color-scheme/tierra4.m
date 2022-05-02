92f29071c16d2990d30df5bdffdf2508ebeb3a19
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2022-05-02 03:54:56 -0300

    Mejorando intercambio de color theme

---
+++ color-scheme/tierra4.m
@@ -0,0 +1,6 @@
+ColorScheme1=[ 43, 79, 96]/255;
+ColorScheme2=[189,199,201]/255;
+ColorScheme3=[234,211,203]/255;
+ColorScheme4=[132, 84,96]/255;
+ColorScheme5=[120,120,120]/255;
+ColorScheme6=[240,240,240]/255;
