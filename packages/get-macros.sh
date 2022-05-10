4efe53c13b6840b9bdc795b77b9284a5ff7b4736
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2022-05-10 12:36:39 -0300

    Ordenando codigo

---
+++ packages/get-macros.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+curl -O https://raw.githubusercontent.com/trucomanx/catalographic-card/main/catalographic-card.sty
+curl -O https://raw.githubusercontent.com/trucomanx/separator-rule/main/separator-rule.sty
+curl -O https://raw.githubusercontent.com/trucomanx/math-macros/main/math-macros.sty
+#curl -O https://raw.githubusercontent.com/trucomanx/env-theorem-pictbox/main/env-theorem-pictbox.sty
+#curl -O https://raw.githubusercontent.com/trucomanx/env-theorem-pictlin/main/env-theorem-pictlin.sty
