8bd7e3f2a72c05f74a2678c25eeffc694dc31429
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2023-08-24 18:18:04 -0300

    mejorando codigo

--- packages/get-macros.sh
+++ packages/get-macros.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
 
-curl -O https://raw.githubusercontent.com/trucomanx/catalographic-card/main/catalographic-card.sty
-curl -O https://raw.githubusercontent.com/trucomanx/separator-rule/main/separator-rule.sty
-curl -O https://raw.githubusercontent.com/trucomanx/math-macros/main/math-macros.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-theorem-pictbox/main/env-theorem-pictbox.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-theorem-simple/main/env-theorem-simple.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-theorem-pictlin/main/env-theorem-pictlin.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-citationbox-simple/main/env-citationbox-simple.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-equationbox-simple/main/env-equationbox-simple.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-bulletjournalround/main/env-bulletjournalround.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-bulletjournalarrow/main/env-bulletjournalarrow.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-bulletjournalpicture/main/env-bulletjournalpicture.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-bulletjournalitem/main/env-bulletjournalitem.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-notebox-simple/main/env-notebox-simple.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-informationbox-round/main/env-informationbox-round.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-elaborationbox-ripple/main/env-elaborationbox-ripple.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-elaborationbox-formal/main/env-elaborationbox-formal.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-elaborationbox-notehor/main/env-elaborationbox-notehor.sty
-curl -O https://raw.githubusercontent.com/trucomanx/env-phrasebox-tab/main/env-phrasebox-tab.sty
+curl https://sourceforge.net/p/latex-macros-packages/catalographic-card/ci/main/tree/catalographic-card.sty?format=raw -o catalographic-card.sty
 
+curl https://sourceforge.net/p/latex-macros-packages/separator-rule/ci/main/tree/separator-rule.sty?format=raw -o separator-rule.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/math-macros/ci/main/tree/math-macros.sty?format=raw -o math-macros.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-theorem-pictbox/ci/main/tree/env-theorem-pictbox.sty?format=raw -o env-theorem-pictbox.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-theorem-simple/ci/main/tree/env-theorem-simple.sty?format=raw -o env-theorem-simple.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-theorem-pictlin/ci/main/tree/env-theorem-pictlin.sty?format=raw -o env-theorem-pictlin.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-citationbox-simple/ci/main/tree/env-citationbox-simple.sty?format=raw -o env-citationbox-simple.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-equationbox-simple/ci/main/tree/env-equationbox-simple.sty?format=raw -o env-equationbox-simple.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-bulletjournalround/ci/main/tree/env-bulletjournalround.sty?format=raw -o env-bulletjournalround.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-bulletjournalarrow/ci/main/tree/env-bulletjournalarrow.sty?format=raw -o env-bulletjournalarrow.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-bulletjournalpicture/ci/main/tree/env-bulletjournalpicture.sty?format=raw -o env-bulletjournalpicture.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-bulletjournalitem/ci/main/tree/env-bulletjournalitem.sty?format=raw -o env-bulletjournalitem.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-notebox-simple/ci/main/tree/env-notebox-simple.sty?format=raw -o env-notebox-simple.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-informationbox-round/ci/main/tree/env-informationbox-round.sty?format=raw -o env-informationbox-round.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-elaborationbox-ripple/ci/main/tree/env-elaborationbox-ripple.sty?format=raw -o env-elaborationbox-ripple.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-elaborationbox-formal/ci/main/tree/env-elaborationbox-formal.sty?format=raw -o env-elaborationbox-formal.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-elaborationbox-notehor/ci/main/tree/env-elaborationbox-notehor.sty?format=raw -o env-elaborationbox-notehor.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-phrasebox-tab/ci/main/tree/env-phrasebox-tab.sty?format=raw -o env-phrasebox-tab.sty
+
+curl https://sourceforge.net/p/latex-macros-packages/env-phrasebox-ripple/ci/master/tree/env-phrasebox-ripple.sty?format=raw -o env-phrasebox-ripple.sty
 
